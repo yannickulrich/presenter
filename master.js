b53fafa125429e7131f61d03c114668840207607
aengelke <alexisengelke@googlemail.com>
2013-04-24 21:20:41 +0300

    Fix issue #9

--- master.js
+++ master.js
@@ -2,79 +2,76 @@
 
 PDFJS.workerSrc = 'PDFJS/worker_loader.js';
 var App = {
-	init: function(pdfURL) {
-		// DO SOMETHING!!
-		var self = this;
-		this.sketchpad = Raphael.sketchpad("drawing", {
-                width: 800,
-                height: 600,
-                editing: false
-            });
-        PDFJS.getDocument(pdfURL).then(function(pdf)
-        {
+    init: function (pdfURL) {
+        // DO SOMETHING!!
+        var self = this;
+        PDFJS.getDocument(pdfURL).then(function (pdf) {
             self.pdfObj = pdf;
-            pdf.getPage(self.pageNumber).then(function(page) {
+            pdf.getPage(self.pageNumber).then(function (page) {
                 self.pageObj = page;
-                
+
                 var viewport = page.getViewport(1);
-                
-                var scaleWidth  = window.innerWidth / viewport.width;
-                var scaleHeight = window.innerHeight / viewport.height;
-                
+
+                var scaleWidth = (window.innerWidth - 3) / viewport.width;
+                var scaleHeight = (window.innerHeight - 3) / viewport.height;
+
                 self.scale = ((scaleWidth > scaleHeight) ? scaleHeight : scaleWidth);
                 self.display();
+                self.sketchpad = Raphael.sketchpad("drawing", {
+                    width: 800,
+                    height: 600,
+                    editing: false
+                });
+                $("#drawing").attr("style", "").css("-webkit-transform", "scaleX(" + (viewport.width * self.scale / 800.0) + ") scaleY(" + (viewport.height * self.scale / 600.0) + ")");
+                $("#drawing, #drawing svg").css("position", "absolute");
+	            window.setInterval("App.update()", 500);
             });
-            
-            
+
         });
+            
+
     },
-    update : function()
-    {
-        self = this;
+    update: function () {
+        var self = this;
         $.ajax({
             url: "news.php?read",
             cache: false
-        }).done(function( msg )
-        {
+        }).done(function (msg) {
             var cmds = msg.split(">");
-            for (var i = 0; i < cmds.length; i++)
-            {
+            for (var i = 0; i < cmds.length; i++) {
                 var cmd = cmds[i].split("|||");
-                if (cmd[0] == "Movepage")
-                {
+                if (cmd[0] == "Movepage") {
                     self.pageNumber = parseInt(cmd[1]) + 1;
-                    self.pdfObj.getPage(self.pageNumber).then(function(page) { self.pageObj = page; self.display() });
+                    self.pdfObj.getPage(self.pageNumber).then(function (page) { self.pageObj = page; self.display() });
                 }
-                else if (cmd[0] == "Draw")
-                {
+                else if (cmd[0] == "Draw") {
                     self.sketchpad.json(cmd[1]);
                 }
             }
         });
     },
-    display : function()
-    {
-        var viewport = this.pageObj.getViewport(this.scale);        
+    display: function () {
+        var viewport = this.pageObj.getViewport(this.scale);
         var canvas = document.getElementById('slide');
         var context = canvas.getContext('2d');
         canvas.height = viewport.height;
         canvas.width = viewport.width;
         
+
         var renderContext = {
             canvasContext: context,
             viewport: viewport
         };
         this.pageObj.render(renderContext);
     },
-    pdfObj : 0,
-	pageNumber : 1,
-	pageObj : 0,
-	scale : 1,
-	sketchpad : null
+    pdfObj: 0,
+    pageNumber: 1,
+    pageObj: 0,
+    scale: 1,
+    sketchpad: null
 };
 
 
 $(document).ready(function() {
 	App.init("content/slides.pdf");
-	window.setInterval("App.update()", 500);
 });
