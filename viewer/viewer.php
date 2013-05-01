5d61fa04f6d50780a097ff078175f3f63687e0c5
Yannick Ulrich <yannick.ulrich@collinproject.de>
2013-05-01 13:39:31 +0200

    Issue #33: A design propose

--- viewer/viewer.php
+++ viewer/viewer.php
@@ -36,12 +36,33 @@
         <script type="text/javascript" src="lib/canvg.js"></script> 
 
         <script type="text/javascript" src="viewer/viewer.js"></script>
+        
+        <style type="text/css">
+            #laserpointer
+{
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    background-color: red;
+    border-radius: 50%;
+    
+    
+    background: -moz-radial-gradient(center, ellipse cover, rgba(255,255,255,1) 2%, rgba(255,255,255,1) 20%, rgba(255,255,255,0.98) 22%, rgba(255,0,0,0.74) 41%, rgba(255,0,0,0) 100%);
+background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(2%,rgba(255,255,255,1)), color-stop(20%,rgba(255,255,255,1)), color-stop(22%,rgba(255,255,255,0.98)), color-stop(41%,rgba(255,0,0,0.74)), color-stop(100%,rgba(255,0,0,0)));
+background: -webkit-radial-gradient(center, ellipse cover, rgba(255,255,255,1) 2%,rgba(255,255,255,1) 20%,rgba(255,255,255,0.98) 22%,rgba(255,0,0,0.74) 41%,rgba(255,0,0,0) 100%);
+background: -o-radial-gradient(center, ellipse cover, rgba(255,255,255,1) 2%,rgba(255,255,255,1) 20%,rgba(255,255,255,0.98) 22%,rgba(255,0,0,0.74) 41%,rgba(255,0,0,0) 100%);
+background: -ms-radial-gradient(center, ellipse cover, rgba(255,255,255,1) 2%,rgba(255,255,255,1) 20%,rgba(255,255,255,0.98) 22%,rgba(255,0,0,0.74) 41%,rgba(255,0,0,0) 100%);
+background: radial-gradient(ellipse at center, rgba(255,255,255,1) 2%,rgba(255,255,255,1) 20%,rgba(255,255,255,0.98) 22%,rgba(255,0,0,0.74) 41%,rgba(255,0,0,0) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#00ff0000',GradientType=1 );
+
+}
+        </style>
     </head>
     <body style="margin:0;padding:0;">
         <a href="javascript:App.download()">Download this slide</a>
         <div id="drawing"></div>
         <canvas id="slide" style="border:1px solid black;"></canvas>
-        <div id="laserpointer" style="position: absolute;    width: 10px;    height: 10px;    background-color: red; border-radius: 50%;"></div>
+        <div id="laserpointer" ></div>
         
         <canvas style="display:none;" id="canvasForOutput" width="1000px" height="600px"></canvas> 
         
